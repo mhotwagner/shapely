469688bee167ff973e427a7fe51947f47e56b54a
Michael Hotwagner <mhotwagner@gmail.com>
2018-02-19 22:44:02 -0500

    Update manage.py to make test running easier

--- manage.py
+++ manage.py
@@ -12,4 +12,8 @@ if __name__ == "__main__":
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    if len(sys.argv) > 2 and sys.argv[1] == 'test':
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'shapely.settings.testing'
+    
     execute_from_command_line(sys.argv)
