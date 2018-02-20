674aa4481a5b02e10b8f2a64a5180adb21c2e0b8
Michael Hotwagner <mhotwagner@gmail.com>
2018-02-20 01:53:23 -0500

    Add partial update and delete routes

--- apps/shapes/tests/test_views.py
+++ apps/shapes/tests/test_views.py
@@ -3,8 +3,8 @@ import json
 from django.test import TestCase, Client
 from django.urls import reverse
 from rest_framework.status import (
-    HTTP_200_OK, HTTP_201_CREATED,
-    HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+    HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT,
+    HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND,
 )
 
 from ..models import Shape
@@ -78,7 +78,7 @@ class TestShapeViewSet(TestCase):
         )
         self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
 
-    def test_update_route_returns_200_with_all_partial_data(self):
+    def test_update_route_returns_200_with_all_data(self):
         shape = ShapeFactory.create(vertices=3)
         response = self.client.put(
             reverse('shapes:shape-detail', kwargs={'pk': shape.id}),
@@ -86,4 +86,38 @@ class TestShapeViewSet(TestCase):
             content_type='application/json',
         )
         self.assertEqual(response.status_code, 200)
+
+    def test_update_route_updates_object(self):
+        shape = ShapeFactory.create(vertices=3)
+        self.client.put(
+            reverse('shapes:shape-detail', kwargs={'pk': shape.id}),
+            json.dumps({'vertices': 4, 'name': 'test'}),
+            content_type='application/json',
+        )
+        fetched_shape = Shape.objects.get(id=shape.id)
+        self.assertEqual(fetched_shape.vertices, 4)
+
+    def test_partial_update_route_returns_200_with_valid_partial_data(self):
+        shape = ShapeFactory.create(vertices=3)
+        response = self.client.patch(
+            reverse('shapes:shape-detail', kwargs={'pk': shape.id}),
+            json.dumps({'vertices': 4}),
+            content_type='application/json',
+        )
+        self.assertEqual(response.status_code, HTTP_200_OK)
+
+    def test_delete_route_returns_400_for_invalid_id(self):
+        response = self.client.delete(reverse('shapes:shape-detail', kwargs={'pk': 999}))
+        self.assertEqual(response.status_code, HTTP_404_NOT_FOUND)
+
+    def test_delete_route_returns_400_for_invalid_id(self):
+        shape = ShapeFactory.create(vertices=3)
+        response = self.client.delete(reverse('shapes:shape-detail', kwargs={'pk': shape.id}))
+        self.assertEqual(response.status_code, HTTP_204_NO_CONTENT)
+
+    def test_delete_route_deletes_object(self):
+        shape = ShapeFactory.create(vertices=3)
+        response = self.client.delete(reverse('shapes:shape-detail', kwargs={'pk': shape.id}))
+        with self.assertRaises(Shape.DoesNotExist):
+            Shape.objects.get(id=shape.id)
 
